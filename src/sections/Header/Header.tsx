b805a59ad38b202a0cb0c5be1803fc367e9d611a
aziznqd <aziznaqdiyev@gmail.com>
2026-09-04 20:34:31 +0400

    responsivity added to benefits section

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -5,7 +5,7 @@ import './Header.css'
 export default function Header() {
     return (
         <section className="header-section">
-            <h1 className="font-crimson font-normal text-[clamp(76px,100vw,160px)] text-center leading-none">Browse <br className="lg:hidden"/> everything.</h1>
+            <h1 className="font-crimson font-normal text-[clamp(76px,10vw,160px)] text-center leading-none">Browse <br className="lg:hidden"/> everything.</h1>
             <div className='header-image-box'>
                 <img src={iphone} alt="iphone" className='block md:hidden'/>
                 <img src={ipad} alt="ipad" className='hidden md:block'/>
